efff8e91f2f7cf29f73a5aa8749ec765f41eced9
rachel <rachel.balanon@gmail.com>
2018-10-23 00:28:14 -0400

    second commit

--- script.js
+++ script.js
@@ -1,34 +1,27 @@
 "use strict";
 
 $(document).ready(() => {
-    $(document).on("mouseenter", ".table", (event) => {
+    // Changes opacity of tables 
+    $(document).on("mouseenter", ".available", (event) => {
         $(event.target).fadeTo(800, 0.2);
     });
-    $(document).on("mouseleave", ".table", (event) => {
+    $(document).on("mouseleave", ".available", (event) => {
         $(event.target).fadeTo(800, 1);
     });
-
-    $(".table").on("click", () => {
+    // Shows form when a table is clicked
+    $(".available").on("click", () => {
         $(".form-container").show(300);
     });
-
+    // Closes form with x
     $(".material-icons").on("click", () => {
         $(".form-container").hide(300);
     });
-
-    // $(".save-btn").on("click", (event) => {
-    //     $(event.target).
-    // });
-
+    // Closes/saves form and changes tables to reserved 
     $(".save-btn").on("click", () => {
         $(".form-container").hide(300);
         $(".available").addClass(".reserved").removeClass(".available");
     });
 
-    // $(document).on("click", "#it", (event) => {
-    //     $(event.target).addClass("red");
-    //     //selects elements that are green and turns them yellow when you click on element with class of "it"
-    //     $(".green").addClass("yellow").removeClass("green");
-    // });
-
+    // Table Number
+    // console.log($(".available"));
 });
